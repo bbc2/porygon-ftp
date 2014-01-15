d43fbaf6358440f9aad3b1885ca625f503c25470
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2014-01-15 20:32:34 +0100

    ftp_retry: Catch EOFError
    Yet another error that can occur during the traversal of the FTP server.

--- ftp_retry.py
+++ ftp_retry.py
@@ -3,13 +3,12 @@ import socket
 from retry import with_max_retries
 
 MAX_RETRIES = 10
-EXCEPTIONS_TO_CATCH = (socket.timeout,)
+EXCEPTIONS_TO_CATCH = (socket.timeout, EOFError)
 
 class FTP_Retry(ftplib.FTP):
     @with_max_retries(MAX_RETRIES, EXCEPTIONS_TO_CATCH)
     def mlsd(self, *args, **kwargs):
         return [f for f in super(FTP_Retry, self).mlsd(*args, **kwargs)]
-
 
 for name in ['pwd', 'cwd']:
     method = getattr(FTP_Retry, name)
