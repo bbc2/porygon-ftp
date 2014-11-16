554a9cb63461abe3af24c678daace150804165f8
Bertrand Bonnefoy-Claudet <bertrandbc@gmail.com>
2014-11-16 22:53:34 +0100

    Saner default settings: longer delays

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ SCAN_TIMEOUT = 20
 MAX_SCAN_TASKS = 1000
 
 # Interval between scans
-SCAN_INTERVAL = 2 * 60
+SCAN_INTERVAL = 10 * 60
 
 # Offline time after which a server is forgotten
-OFFLINE_DELAY = 2 * 3600
+OFFLINE_DELAY = 24 * 3600
 
 # Timeout for the connection to an FTP server during indexation
 INDEX_TIMEOUT = 30
@@ -20,7 +20,7 @@ INDEX_TIMEOUT = 30
 MAX_INDEX_TASKS = 2
 
 # Minimum interval between index tasks on a given host
-INDEX_INTERVAL = 5 * 60
+INDEX_INTERVAL = 4 * 3600
 
 # Maximum number of FTP errors allowed during the indexation of a server
 MAX_INDEX_ERRORS = 10
